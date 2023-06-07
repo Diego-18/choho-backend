a32a528c7a6b6235ec83b80a21e608a0e3cb330c
Diego Chavez <ingdiegochavez18@gmail.com>
2023-06-07 12:14:43 -0500

    feat: add-routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use
+    App\Http\Controllers\{ProviderController, BranchController, UserController, DepartmentController, CityController};
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +17,48 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::controller(ProviderController::class)->group(function () {
+    Route::post('/providers', 'getAllProviders');
+    Route::post('/provider', 'createProvider');
+    Route::put('/provider/{id}', 'updateProvider');
+    Route::delete('/provider/{id}', 'deleteProvider');
+});
+
+Route::controller(UserController::class)->group(function () {
+    Route::get('/users', 'getAllUsers');
+    Route::post('/user', 'createUser');
+});
+
+Route::controller(BranchController::class)->group(function () {
+    Route::get('/branchs', 'getAllBranchs');
+    Route::post('/branch', 'createBranch');
+    Route::put('/branch/{id}', 'updateBranch');
+    Route::delete('/branch/{id}', 'deleteBranch');
+});
+
+Route::controller(ProductController::class)->group(function () {
+    Route::get('/products', 'getAllProducts');
+    Route::post('/product', 'createProduct');
+    Route::put('/product/{id}', 'updateProduct');
+    Route::delete('/product/{id}', 'deleteProduct');
+});
+
+Route::controller(OrderController::class)->group(function () {
+    Route::get('/orders', 'getAllOrders');
+    Route::post('/order', 'createOrder');
+    Route::put('/order/{id}', 'updateOrder');
+    Route::delete('/order/{id}', 'deleteOrder');
+});
+
+
+Route::controller(DepartmentController::class)->group(function () {
+    Route::get('/departments', 'getAllDepartments');
+});
+
+Route::controller(CityController::class)->group(function () {
+    Route::get('/cities', 'getAllCities');
 });
