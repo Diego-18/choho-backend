adaabe8f0e1c9609420d52335271ea257f218947
Diego Chavez <ingdiegochavez18@gmail.com>
2023-06-07 23:29:07 -0500

    feat: add-login-with-Sanctum

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
@@ -82,5 +83,97 @@ class UserController extends Controller
                 500
             );
         }
+    }
+
+     public function logIn(Request $request): Response
+    {
+        try {
+            $validator = Validator::make($request->all(), [
+                'email' => 'required|string|email|max:255',
+                'password' => 'required|string|min:6',
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json(
+                    [
+                        'success' => false,
+                        'message' => 'Faltan datos de entrada.',
+                        'error' => $validator->errors(),
+                    ],
+                    406
+                );
+            }
+
+            if (!Auth::attempt($request->all())) {
+                return $this->error('Credentials not match', 401);
+            }
+            $user = User::where('email', $request['email'])->firstOrFail();
+
+            if ($user) {
+               $token = $user->createToken('auth_token')->plainTextToken;
+
+                return response()->json(
+                    [
+                        'res' => true,
+                        'token' => $token,
+                        'user_id' => $user->id,
+                        'user_name' => $user->name,
+                    ],
+                    200
+                );
+            } else {
+                return response()->json(
+                    [
+                        'success' => false,
+                        'error' => $validator->errors(),
+                        'status' => 406,
+                    ],
+                    406
+                );
+            }
+        } catch (\Exception $error) {
+            return Response()->json(
+                [
+                    'message' => 'Invalid Credentials',
+                    'error' => $error->getMessage(),
+                    'line' => $error->getLine(),
+                    'file' => $error->getFile(),
+                ],
+                500
+            );
+        }
+    }
+
+    public function logOut(User $user): Response
+    {
+        try {
+            $user->tokens()->delete();
+
+            return response()->json(
+                [
+                    'res' => true,
+                    'msg' => 'Close session successfully',
+                ],
+                200
+            );
+        } catch (\Exception $error) {
+            return Response()->json(
+                [
+                    'message' => $error->getMessage(),
+                    'line' => $error->getLine(),
+                    'file' => $error->getFile(),
+                ],
+                500
+            );
+        }
+    }
+
+    public function me(Request $request)
+    {
+        $user_logged = $request->user();
+        $id = $user_logged['id'];
+        $user = User::find($id);
+
+        return $user;
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -30,43 +30,49 @@ use
 // });
 
 Route::controller(CustomerController::class)->group(function () {
-    Route::post('/customers', 'getAllCustomers');
-    Route::post('/customer', 'createCustomer');
-    Route::put('/customer/{id}', 'updateCustomer');
-    Route::delete('/customer/{id}', 'deleteCustomer');
+    Route::post('/customers', 'getAllCustomers')->middleware('auth:sanctum');
+    Route::post('/customer', 'createCustomer')->middleware('auth:sanctum');
+    Route::put('/customer/{id}', 'updateCustomer')->middleware('auth:sanctum');
+    Route::delete('/customer/{id}', 'deleteCustomer')->middleware('auth:sanctum');
 });
 
+// Route::controller(UserController::class)->group(function () {
+//     Route::get('/users', 'getAllUsers');
+//     Route::post('/user', 'createUser');
+// });
+
 Route::controller(UserController::class)->group(function () {
-    Route::get('/users', 'getAllUsers');
-    Route::post('/user', 'createUser');
+    Route::post('/login', 'logIn');
+    Route::post('/logout', 'logOut');
+    Route::get('/user', 'me')->middleware('auth:sanctum');
 });
 
 Route::controller(BranchController::class)->group(function () {
-    Route::post('/branches', 'getAllBranchs');
-    Route::post('/branch', 'createBranch');
-    Route::put('/branch/{id}', 'updateBranch');
-    Route::delete('/branch/{id}', 'deleteBranch');
+    Route::post('/branches', 'getAllBranchs')->middleware('auth:sanctum');
+    Route::post('/branch', 'createBranch')->middleware('auth:sanctum');
+    Route::put('/branch/{id}', 'updateBranch')->middleware('auth:sanctum');
+    Route::delete('/branch/{id}', 'deleteBranch')->middleware('auth:sanctum');
 });
 
 Route::controller(ProductController::class)->group(function () {
-    Route::post('/products', 'getAllProducts');
-    Route::post('/product', 'createProduct');
-    Route::put('/product/{id}', 'updateProduct');
-    Route::delete('/product/{id}', 'deleteProduct');
+    Route::post('/products', 'getAllProducts')->middleware('auth:sanctum');
+    Route::post('/product', 'createProduct')->middleware('auth:sanctum');
+    Route::put('/product/{id}', 'updateProduct')->middleware('auth:sanctum');
+    Route::delete('/product/{id}', 'deleteProduct')->middleware('auth:sanctum');
 });
 
 Route::controller(OrderController::class)->group(function () {
-    Route::post('/orders', 'getAllOrders');
-    Route::post('/order', 'createOrder');
-    Route::put('/order/{id}', 'updateOrder');
-    Route::delete('/order/{id}', 'deleteOrder');
+    Route::post('/orders', 'getAllOrders')->middleware('auth:sanctum');
+    Route::post('/order', 'createOrder')->middleware('auth:sanctum');
+    Route::put('/order/{id}', 'updateOrder')->middleware('auth:sanctum');
+    Route::delete('/order/{id}', 'deleteOrder')->middleware('auth:sanctum');
 });
 
 
 Route::controller(DepartmentController::class)->group(function () {
-    Route::post('/departments', 'getAllDepartments');
+    Route::post('/departments', 'getAllDepartments')->middleware('auth:sanctum');
 });
 
 Route::controller(CityController::class)->group(function () {
-    Route::post('/cities', 'getAllCities');
+    Route::post('/cities', 'getAllCities')->middleware('auth:sanctum');
 });
